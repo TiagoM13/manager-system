8c6af6e7451f32666ee773718ebe78970a254e43
TiagoM13 <thyagomotha2000@gmail.com>
2024-05-10 11:19:21 -0300

    feat: add tests and configs

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -4,18 +4,24 @@ type Props = {
   imageUrl: string;
   name: string;
   small?: boolean;
-  color?: 'light' | 'dark';
+  color: 'light' | 'dark';
 };
 
 export const Avatar: React.FC<Props> = ({ imageUrl, name, small, color }) => {
   return (
     <div
+      data-testid="avatar-container"
       className={`
       ${small ? 'size-[45px] ' : 'size-16'}
       ${color === 'light' ? 'border-white' : 'border-slate-700'}
       border-[1.5px] rounded-full`}
     >
-      <img src={imageUrl} alt={name} className="rounded-full" />
+      <img
+        data-testid="image-tag"
+        src={imageUrl}
+        alt={name}
+        className="rounded-full"
+      />
     </div>
   );
 };
