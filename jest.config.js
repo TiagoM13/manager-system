9c65acc29766bd249f38413d795311e5af84d475
TiagoM13 <thyagomotha2000@gmail.com>
2024-05-13 12:07:55 -0300

    feat: add imports settings in jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest/setup.ts'],
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/jest/__mocks__/setupMock.js',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transformIgnorePatterns: ['/node_modules/'],
 };
