40eb464eb35b037128088f0c7c26700733107663
TiagoM13 <thyagomotha2000@gmail.com>
2024-09-27 11:11:52 -0300

    styles: fix lint

--- src/interfaces/patients.ts
+++ src/interfaces/patients.ts
@@ -25,8 +25,7 @@ export interface IPatient {
   updated_at?: Date;
 }
 
-// eslint-disable-next-line prettier/prettier
-export interface IPatientForm extends Omit<IPatient, 'status'> { }
+export interface IPatientForm extends Omit<IPatient, 'status'> {}
 
 export interface IPatientFilters {
   name?: string;
